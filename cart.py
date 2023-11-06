ad5491b17251aa58788751d2964a9bdd88640a84
Pedro C <pedro.jcaceresl@gmail.com>
2023-11-06 07:48:17 -0300

    Fix cart checkout

--- cart.py
+++ cart.py
@@ -20,7 +20,10 @@ class Cart:
 
     def check_out(self):
         if self.owner.wallet.balance < self.total_amount():
+            print("Insufficient balance. Checkout failed.")
+        else:
             for item in self.items:
-                item.owner.wallet.balance += item.price # Transfer the money
-                item.owner = self.owner # Transfer ownership
-            self.items = [] # Empty the cart
+                item.owner.wallet.balance += item.price  # Transfer the money to item owner
+                self.owner.wallet.balance -= item.price
+                item.owner = self.owner  # Transfer ownership to cart owner
+            self.items = []  # Empty the cart
